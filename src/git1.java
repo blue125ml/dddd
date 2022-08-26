5849c5e7126ba02c5512ab6ba7f4506e2097ec56
yongwook <blue125ml@naver.com>
2022-08-26 21:58:07 +0900

    seconde commit

--- src/git1.java
+++ src/git1.java
@@ -1,2 +1,38 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.Scanner;
+
 public class git1 {
-}
+
+    public static void main(String[] args) {
+        int value ;
+        Scanner sc= new Scanner(System.in);
+
+        int num= sc.nextInt();
+        int cnt = 1;
+
+
+        for(int i=1; i <= num; i++){
+            System.out.println();
+            for(int j=1; j<=i; j++){
+
+                    System.out.print(cnt);
+                    cnt ++;
+                    while(cnt==10) {
+                        cnt=0;
+                        System.out.print(cnt);
+                        cnt++;
+                    }
+
+                }
+
+            }
+
+
+        }
+
+
+
+
+    }
+
